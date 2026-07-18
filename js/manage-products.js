9e3faae5902db9dec82066d5638f8429dde31213
vikscan732-del <vikscan732@gmail.com>
2026-07-18 12:13:07 +0530

    manage-products.js

--- js/manage-products.js
+++ js/manage-products.js
@@ -8,28 +8,53 @@ import {
 const list = document.getElementById("products");
 
 async function loadProducts() {
-  list.innerHTML = "<h3 style='text-align:center'>Loading products...</h3>";
 
-  const snapshot = await getDocs(collection(db, "products"));
+  list.innerHTML =
+  "<h3 style='text-align:center'>Loading products...</h3>";
+
+  const snapshot = await getDocs(
+    collection(db, "products")
+  );
 
   list.innerHTML = "";
 
   if (snapshot.empty) {
-    list.innerHTML = "<h3>No products found</h3>";
+    list.innerHTML =
+    "<h3>No products found</h3>";
     return;
   }
 
-  snapshot.forEach((doc) => {
-    const p = doc.data();
+  snapshot.forEach((item) => {
+
+    const p = item.data();
 
     list.innerHTML += `
-      <div class="card">
-        <h3>${p.name}</h3>
-        <p>₹ ${p.price}</p>
-        <p>${p.category}</p>
-      </div>
+
+    <div class="card">
+
+      <h3>${p.name}</h3>
+
+      <p><b>₹ ${p.price}</b></p>
+
+      <p>${p.category}</p>
+
+      <button onclick="editProduct('${item.id}')">
+        Edit
+      </button>
+
+    </div>
+
     `;
+
   });
+
+}
+
+window.editProduct = function(id){
+
+  window.location.href =
+  "edit-product.html?id=" + id;
+
 }
 
 loadProducts();
