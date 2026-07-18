5653f197b285663b4ff548badac8565853d4c47a
vikscan732-del <vikscan732@gmail.com>
2026-07-18 14:58:01 +0530

    app.js

--- js/app.js
+++ js/app.js
@@ -1,31 +1,11 @@
 // Firebase Imports
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-
+import { db } from "./firebase.js";
 import {
-getFirestore,
 collection,
 getDocs
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
-// ===============================
-// FIREBASE CONFIG
-// ===============================
 
-const firebaseConfig = {
-
-apiKey: "YOUR_API_KEY",
-
-authDomain: "YOUR_PROJECT.firebaseapp.com",
-
-projectId: "YOUR_PROJECT",
-
-storageBucket: "YOUR_PROJECT.firebasestorage.app",
-
-messagingSenderId: "YOUR_SENDER_ID",
-
-appId: "YOUR_APP_ID"
-
-};
 
 // ===============================
 
