5b3b8e33c2cf5cbad322acfd77e88cb10c20c313
vikscan732-del <vikscan732@gmail.com>
2026-07-28 15:16:40 +0530

    info.js

--- js/info.js
+++ js/info.js
@@ -1,9 +1,31 @@
-// ================= HISTORY =================
+// ==============================
+// OPEN HISTORY PAGE
+// ==============================
 
 document.getElementById("petrolCard").onclick=()=>{
-    location.href="history.html?type=petrol";
+location.href="history.html?type=petrol";
 };
 
 document.getElementById("dieselCard").onclick=()=>{
-    location.href="history.html?type=diesel";
+location.href="history.html?type=diesel";
+};
+
+document.getElementById("cngCard").onclick=()=>{
+location.href="history.html?type=cng";
+};
+
+document.getElementById("autogasCard").onclick=()=>{
+location.href="history.html?type=autogas";
+};
+
+document.getElementById("lpgCard").onclick=()=>{
+location.href="history.html?type=lpg";
+};
+
+document.getElementById("goldCard").onclick=()=>{
+location.href="history.html?type=gold";
+};
+
+document.getElementById("silverCard").onclick=()=>{
+location.href="history.html?type=silver";
 };
