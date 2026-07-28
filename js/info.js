e0bd161fa5941d0c6d95070a23d8aad35a2f51e6
vikscan732-del <vikscan732@gmail.com>
2026-07-28 14:49:47 +0530

    info.js

---
+++ js/info.js
@@ -0,0 +1,9 @@
+// ================= HISTORY =================
+
+document.getElementById("petrolCard").onclick=()=>{
+    location.href="history.html?type=petrol";
+};
+
+document.getElementById("dieselCard").onclick=()=>{
+    location.href="history.html?type=diesel";
+};
